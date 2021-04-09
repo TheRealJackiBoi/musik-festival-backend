10401c04c7c971038f6a2fe9aeae9b70e2b3f692
TheRealJackiBoi <jslam@oulund.dk>
2021-04-09 12:07:13 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 8000;
+//const index = require("./routes/index");
+
+const app = express();
+//app.use(index);
+
+app.use(express.static('public'));
+
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+    cors:{
+        origin: "http://localhost:3000"
+    }
+});
+
+
+
+app.get('/', function(request, response) {
+    response.send(counter.toString());
+  });
+
+
+io.on("connection", (socket) => {
+
+    console.log("New client connected");
+    socket.emit("NewMessage", {sender: "Bob", message: "Hello this is chat"});
+    socket.emit("NewMessage", {sender: "Svend21", message: "Hello chat this is svend"});
+    
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+
+    socket.on("addMessage", (data) => {
+       socket.broadcast.emit("NewMessage", data);
+    });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
