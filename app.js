d7dfec5db1e71b832f5d01f4de7bf0499d3172ba
Christian Schwenger <drikdrok02@gmail.com>
2021-05-04 14:33:22 +0200

    Handle voting

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const firebase = require("firebase");
 require("firebase/auth");
 require("firebase/firestore");
 
+let playlistId = "PL7zsB-C3aNu1_Z7Qd-Qcbf2_U2YqUzK31";
+let playlistPageToken = "CAUQAA";
+
+
+let currentVideoLength;
+
 const firebaseConfig = {
     apiKey: "AIzaSyAqTzo-Dw0BRpn-CtcoMKwBTinuyMcw05w",
     authDomain: "musik-festival.firebaseapp.com",
@@ -25,14 +31,19 @@ const auth = firebase.auth();
 
 const timeInfoRef = db.collection("timeInfo");
 const videoInfoRef = db.collection("videoInfo");
+const votingRef = db.collection("voting");
 
 
 app.listen(port, () =>{
+
+
     console.log("Listing on " + port);
 
+    start();
     setInterval(loop, 2000); //Loop every second
 
-    newVideo("fZMRc-UyPm0");
+    //newVideo("fZMRc-UyPm0");
+    //startNewVotingCycle();
 });
 
 
@@ -46,30 +57,112 @@ async function loop(){
 
     timeInfo.time += 2;
 
-    timeInfoRef.doc("info").set(timeInfo).then(ref => console.log("Time: " + timeInfo.time));
+    console.log("Time: " + timeInfo.time)
+
+    if (timeInfo.time >= currentVideoLength){
+        console.log("Video ended. Starting new video");
+        startNextVideo();
+        startNewVotingCycle();
+        timeInfo.time = 0;
+    }
+
+    timeInfoRef.doc("info").set(timeInfo);
+
+
 }
 
 function newVideo(videoId){
 
     let video;
 
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + videoId +"&key=AIzaSyABQyO1Hrn0HCqGgRJFZm-Wm4fqNRFcjO4")
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=" + videoId +"&key=AIzaSyABQyO1Hrn0HCqGgRJFZm-Wm4fqNRFcjO4&")
       .then(res => res.json())
       .then(
         (result) => {
           //result.items[0].snippet.title = result.items[0].snippet.title.replace("(Video)", "");
-            video = result.items[0].snippet;
             
-            
+          //console.log(result.items[0].contentDetails.duration.replace("PT", ""));
+          let minutes = result.items[0].contentDetails.duration.substring(2,3);
+          let seconds = result.items[0].contentDetails.duration.substring(4,10).replace("S", "")
+          
+          currentVideoLength = Number(minutes) * 60 + Number(seconds);
+
+          video = result.items[0].snippet;
             
             videoInfo = {
                 title: video.title,
-                videoId: videoId
+                videoId: videoId,
+                duration: currentVideoLength
             }
-            console.log(videoInfo.title);
+            //console.log(videoInfo.title);
             
             videoInfoRef.doc("info").set(videoInfo);
+            console.log("Playing video: " + video.title)
+            console.log("New video length: " + currentVideoLength);
 
         },
       );
 }
+
+
+function startNewVotingCycle(){
+    fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=PL7zsB-C3aNu1_Z7Qd-Qcbf2_U2YqUzK31&pageToken=" + playlistPageToken + "&key=AIzaSyABQyO1Hrn0HCqGgRJFZm-Wm4fqNRFcjO4")
+        .then(res => res.json())
+          .then(
+            (result) => {
+              //console.log(result);
+
+              playlistPageToken = result.nextPageToken;
+
+                let votingVideoTitles = [];
+                let votingVideoIds = []
+
+                result.items.forEach(video => {
+                   // console.log(video.snippet.resourceId.videoId);
+                    votingVideoTitles.push(video.snippet.title);
+                    votingVideoIds.push(video.snippet.resourceId.videoId);
+                });
+
+               
+                for (let i = 0; i < 5; i++){
+                    let votes = Math.floor(Math.random() * 95);
+                    votingRef.doc("song"+i).set({title: votingVideoTitles[i],  votes: votes, videoId: votingVideoIds[i], index: i});  
+                }
+
+                
+            },
+          );
+}
+
+
+async function startNextVideo(){ //Find most voted video and play it
+    const snapshot = await votingRef.get();
+
+    let mostVotes = 0;
+    let votedVideo = "fZMRc-UyPm0" // Dummy video
+
+    snapshot.forEach(element => { 
+        let entry = element.data();
+        let votes = entry.votes;
+        if (votes > mostVotes){
+            votedVideo = entry.videoId;
+        }
+    });
+
+    newVideo(votedVideo);
+}
+
+async function start(){
+    const snapshot = await videoInfoRef.get();
+
+    snapshot.forEach(element => { 
+        let info = element.data();
+        currentVideoLength = info.duration;
+    });
+    
+    console.log("Servert started");
+    console.log("Current video length: " + currentVideoLength);
+}
+
+
+
